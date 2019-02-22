ca1a0834f744df287e89d51c88fdfa0b499f9cdb
Eduhaydee <webmaster.kdu@gmail.com>
2019-02-22 20:34:05 -0300

    Visitor view details recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_22_212909) do
+ActiveRecord::Schema.define(version: 2019_02_22_231236) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_02_22_212909) do
     t.integer "cook_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "ingredients"
+    t.text "cook_method"
   end
 
 end
