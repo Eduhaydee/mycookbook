58068c95c9eed87b72ff681a0316d8c742b4cb4d
Eduhaydee <webmaster.kdu@gmail.com>
2019-02-22 21:35:21 -0300

    User edit recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,8 +12,20 @@ class RecipesController < ApplicationController
   end
   
   def create
-    @recipe = Recipe.create(params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty,
+    @recipe = Recipe.new(params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty,
                                                         :cook_time, :ingredients, :cook_method))
+    @recipe.save
+    redirect_to @recipe
+  end
+  
+  def edit
+    @recipe = Recipe.find(params[:id]) 
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id]) 
+    @recipe.update(params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty,
+                                                  :cook_time, :ingredients, :cook_method))
     redirect_to @recipe
   end
   
