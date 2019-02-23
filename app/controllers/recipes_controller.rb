022bad0fcff8e2da7bf3a2949f3b4b1b11b0b509
Eduhaydee <webmaster.kdu@gmail.com>
2019-02-22 21:22:06 -0300

    User register recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,5 +6,15 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.create(params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty,
+                                                        :cook_time, :ingredients, :cook_method))
+    redirect_to @recipe
+  end
   
 end
--- spec/feature/visitor_visit_homepage_spec.rb
+++ spec/feature/visitor_visit_homepage_spec.rb
@@ -18,7 +18,7 @@ feature 'Visitor visit homepage' do
     visit root_path
 
     # expectativas do usuário após a ação
-    expect(page).to have_css('h3', text: recipe.title)
+    expect(page).to have_css('h2', text: recipe.title)
     expect(page).to have_css('li', text: recipe.recipe_type)
     expect(page).to have_css('li', text: recipe.cuisine)
     expect(page).to have_css('li', text: recipe.difficulty)
@@ -40,13 +40,13 @@ feature 'Visitor visit homepage' do
     visit root_path
 
     # expectativas do usuário após a ação
-    expect(page).to have_css('h3', text: recipe.title)
+    expect(page).to have_css('h2', text: recipe.title)
     expect(page).to have_css('li', text: recipe.recipe_type)
     expect(page).to have_css('li', text: recipe.cuisine)
     expect(page).to have_css('li', text: recipe.difficulty)
     expect(page).to have_css('li', text: "#{recipe.cook_time} minutos")
 
-    expect(page).to have_css('h3', text: another_recipe.title)
+    expect(page).to have_css('h2', text: another_recipe.title)
     expect(page).to have_css('li', text: another_recipe.recipe_type)
     expect(page).to have_css('li', text: another_recipe.cuisine)
     expect(page).to have_css('li', text: another_recipe.difficulty)
