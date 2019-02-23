552898879c56f5a345368e9b2af642c339efb23d
Eduhaydee <webmaster.kdu@gmail.com>
2019-02-22 22:01:15 -0300

    Validate register and edit recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,10 +12,13 @@ class RecipesController < ApplicationController
   end
   
   def create
-    @recipe = Recipe.new(params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty,
-                                                        :cook_time, :ingredients, :cook_method))
-    @recipe.save
-    redirect_to @recipe
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      flash[:errors] = "Você deve informar todos os dados da receita"
+      render :new
+    end
   end
   
   def edit
@@ -24,9 +27,19 @@ class RecipesController < ApplicationController
 
   def update
     @recipe = Recipe.find(params[:id]) 
-    @recipe.update(params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty,
-                                                  :cook_time, :ingredients, :cook_method))
-    redirect_to @recipe
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      flash[:errors] = "Você deve informar todos os dados da receita"
+      render :edit
+    end
   end
+
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method)
+  end
+  
   
 end
